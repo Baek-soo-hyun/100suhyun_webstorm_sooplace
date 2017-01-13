839c05a56f70ed615ef744c033d8535f97c7caf8
Baek-soo-hyun <100suhyun@naver.com>
2017-01-13 18:00:02 +0900

    2016.01.13 업데이트

--- src/js/common.js
+++ src/js/common.js
@@ -21,5 +21,84 @@ define([
 		});
 	}
 
+
+	function showLoginLayer() {
+		$("#main-bar-login-button").on("click", function() {
+			$("#main-login-layer").show();
+		});
+	}
+	function hideLoginLayer() {
+		$(".main-login-layer-close").on("click", function() {
+			$("#main-login-layer").hide();
+		});
+	}
+
+
+	function showMailLoginLayer() {
+		$(".button-email").on("click", function() {
+			$("#main-login-layer").hide();
+			$("#mail-login-layer").show();
+		});
+	}
+	function hideMailLoginLayer() {
+		$(".mail-login-layer-close").on("click", function() {
+			$("#mail-login-layer").hide();
+			$("#main-login-layer").hide();
+		});
+	}
+
+
+	function showFindPasswordLayer() {
+		$(".mail-login-find-password").on("click", function() {
+			$("#mail-login-layer").hide();
+			$("#find-password-layer").show();
+		});
+	}
+	function hideFindPasswordLayer() {
+		$(".find-password-layer-close, .cancle-button").on("click", function() {
+			$("#find-password-layer").hide();
+			$("#main-login-layer").hide();
+		});
+	}
+
+
+	function showSignUpLayer() {
+		$(".mail-login-layer-signup-button").on("click", function() {
+			$("#find-password-layer").hide();
+			$("#email-signup-layer").show();
+		});
+	}
+	function hideSignUpLayer() {
+		$(".find-password-layer-close, .cancle-button, .email-signup-layer-close").on("click", function() {
+			$("#email-signup-layer").hide();
+			$("#main-login-layer").hide();
+		});
+	}
+
+
+	function checkBox() {
+		$(".remain-check-box").on("click", function() {
+			$(".check-box-active").show();
+		});
+	}
+	function unCheckBox() {
+		$(".check-box-active").on("click", function() {
+			$(".check-box-active").hide();
+			$(".remain-check-box").show();
+		});
+	}
+
+
 	scrollUp();
+	showLoginLayer();
+	hideLoginLayer();
+	showMailLoginLayer();
+	hideMailLoginLayer();
+	showFindPasswordLayer();
+	hideFindPasswordLayer();
+	showSignUpLayer();
+
+	hideSignUpLayer();
+	checkBox();
+	unCheckBox();
 });
