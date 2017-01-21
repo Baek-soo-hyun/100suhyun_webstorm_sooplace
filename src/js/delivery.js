59d6f0475be0ebaf72f93088f534abeb1b16c18a
Baek-soo-hyun <100suhyun@naver.com>
2017-01-22 00:09:34 +0900

    2016.01.21 집

--- src/js/delivery.js
+++ src/js/delivery.js
@@ -7,6 +7,18 @@ require([
 			$(this).addClass("active");
 		});
 	}
+	function showDeliveryLayer() {
+		$(".delivery-manual-text").on("click", function(event) {
+			$(".delivery-manual-layer").show();
+		});
+	}
+	function hideDeliveryLayer() {
+		$(".delivery-manual-layer-box>.big-close-btn").on("click", function(event) {
+			$(".delivery-manual-layer").hide();
+		});
+	}
 
 	activeDeliveryFilter();
+	showDeliveryLayer();
+	hideDeliveryLayer();
 });
